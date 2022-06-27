6f8e76680675d314f6595e7aeb60466e718659c9
Bapon Kar <baponkar600@gmail.com>
2022-06-27 21:00:13 +0530

    fix pause menu click issues

---
+++ Assets/baponkar_main_menu/Scripts/PauseMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GamePause gamePause;
+    public GameObject backgroundImage;
+    public GameObject panelPauseMenu;
+
+    public Animator panelPauseMenuAnimator;
+
+    public AudioSource audio;
+    public AudioClip clip;
+
+    public AudioSource backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //panelPauseMenuAnimator = panelPauseMenu.GetComponent<Animator>();
+        backgroundImage.SetActive(true);
+        panelPauseMenuAnimator.SetBool("Slide", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        backgroundImage.SetActive(true);
+        panelPauseMenuAnimator.SetBool("Slide", true);
+        gamePause.paused = false;
+    }
+
+    public void PlayClickSound()
+    {
+        audio.PlayOneShot(clip);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+         StartCoroutine(WaitAndPlay());
+    }
+
+    IEnumerator WaitAndPlay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+            backgroundMusic.volume = 0.8f;
+            yield return new WaitForSeconds(.1f);
+            backgroundMusic.volume = 0.6f;
+            yield return new WaitForSeconds(.1f);
+            backgroundMusic.volume = 0.4f;
+            yield return new WaitForSeconds(.1f);
+            backgroundMusic.volume = 0.2f;
+            yield return new WaitForSeconds(.1f);
+            backgroundMusic.volume = 0.0f;
+
+            PlayerPrefs.SetString("LoadScene", "MainMenu");
+            SceneManager.LoadScene("LoadingScene");
+        }
+    }
+}
